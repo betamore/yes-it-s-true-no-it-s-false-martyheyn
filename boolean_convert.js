6735278553f4f26978239efd21d80a59272ef7a4
martyheyn <heynmarty@gmail.com>
2021-11-29 19:54:51 -0500

    ternary

--- boolean_convert.js
+++ boolean_convert.js
@@ -1,13 +1,14 @@
 function boolToWord( bool ){
   
-  if(bool == true) {
-    return "Yes"
-  } else if(bool == false) {
-    return "No"
-  } else {
-    return "Input must be a boolean value - true or false"
-  }
+  // if(bool == true) {
+  //   return "Yes"
+  // } else if(bool == false) {
+  //   return "No"
+  // } else {
+  //   return "Input must be a boolean value - true or false"
+  // }
 
+  return bool == true ? "Yes" : (bool == false ? "No" : "Input must be a boolean value - true or false")
 }
 
 
